e8857547d70dc56b8a3a36eacdc6d9f74adcbc3e
Samuel Deuter <SamuelDeuter@gmail.com>
2020-02-18 14:25:13 -0800

    Install and use morgan

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res, next) => {
-  res.end('Hello World');
-});
+app.use(morgan('dev'));
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
