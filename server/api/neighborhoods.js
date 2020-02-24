d717e8262fbe96cefaae42068d47bc676bcbb03a
Samuel Deuter <SamuelDeuter@gmail.com>
2020-02-24 11:52:36 -0800

    write neighborhood routes

--- server/api/neighborhoods.js
+++ server/api/neighborhoods.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('', (req, res) => {
+// POST /api/neighborhoods/
+router.post('', (req, res) => {
   res.end(`endpoint: ${req.baseUrl}`);
+});
+
+// GET /api/neighborhoods/:id
+router.get('/:id', (req, res) => {
+  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+});
+
+// PATCH /api/neighborhoods/:id
+router.patch('/:id', (req, res) => {
+  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+});
+
+// DELETE /api/neighborhoods/:id
+router.delete('/:id', (req, res) => {
+  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+});
+
+// GET /api/neighborhoods/:id/plot
+router.get('/:id/plot', (req, res) => {
+  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`); 
+});
+
+// POST /api/neighborhoods/:id/plot
+router.post('/:id/plot', (req, res) => {
+  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`); 
 });
 
 module.exports = router;
