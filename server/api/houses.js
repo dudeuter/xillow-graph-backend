393bc691a2ed1b777771061b0b8e908924429769
Samuel Deuter <SamuelDeuter@gmail.com>
2020-02-24 11:31:53 -0800

    api routers for houses

--- server/api/houses.js
+++ server/api/houses.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('', (req, res) => {
-  res.end(`endpoint: ${req.baseUrl}`);
+// POST /api/houses/
+router.post('', (req, res) => {
+  res.end(`endpoint: ${req.baseUrl}, ${req.method}`);
+});
+
+// GET /api/houses/:id
+router.get('/:id', (req, res) => {
+  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+});
+
+// PATCH /api/houses/:id
+router.patch('/:id', (req, res) => {
+  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+});
+
+// DELETE /api/houses/:id
+router.delete('/:id', (req, res) => {
+  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+});
+
+// GET /api/houses/:id/plot
+router.get('/:id/plot', (req, res) => {
+  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+});
+
+// POST /api/houses/:id/plot
+router.post('/:id/plot', (req, res) => {
+  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
 });
 
 module.exports = router;
