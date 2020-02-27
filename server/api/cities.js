4972628a59f17f07a2ae8f0cc102c90fc4e4a8c3
Samuel Deuter <SamuelDeuter@gmail.com>
2020-02-27 15:47:13 -0800

    cities api routes

--- server/api/cities.js
+++ server/api/cities.js
@@ -1,29 +1,97 @@
 const express = require('express');
+const { cities } = require('../../database/models/index');
 
 const router = express.Router();
 
 router.post('', (req, res) => {
-  res.end(`endpoint: ${req.method} ${req.baseUrl}`);
+  cities
+    .insert({})
+    .then(() => {
+      res.status(200);
+    })
+    .catch(() => {
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    });
 });
 
 router.get('/:id', (req, res) => {
-  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+  const id = parseInt(req.params.id, 10);
+  cities
+    .getById(id)
+    .then((data) => {
+      res.status(200);
+      res.send(JSON.stringify(data));
+    })
+    .catch(() => {
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    });
 });
 
 router.put('/:id', (req, res) => {
-  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+  const id = parseInt(req.params.id, 10);
+  cities
+    .updateById(id)
+    .then(() => {
+      res.status(200);
+    })
+    .catch(() => {
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    });
 });
 
 router.delete('/:id', (req, res) => {
-  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+  const id = parseInt(req.params.id, 10);
+  cities
+    .deleteById(id)
+    .then(() => {
+      res.status(200);
+    })
+    .catch(() => {
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    });
 });
 
 router.get('/:id/plot', (req, res) => {
-  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+  const id = parseInt(req.params.id, 10);
+  cities
+    .getPlotsById(id)
+    .then((data) => {
+      res.status(200);
+      res.send(JSON.stringify(data));
+    })
+    .catch(() => {
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    });
 });
 
 router.post('/:id/plot', (req, res) => {
-  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+  const id = parseInt(req.params.id, 10);
+  cities
+    .insertPlotById(id, {})
+    .then(() => {
+      res.status(200);
+    })
+    .catch(() => {
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    });
 });
 
 module.exports = router;
