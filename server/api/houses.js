336dd8570545c0f1724fe2a3e7f8fef15302ee88
Samuel Deuter <SamuelDeuter@gmail.com>
2020-02-27 15:06:42 -0800

    implement routes for houses

--- server/api/houses.js
+++ server/api/houses.js
@@ -1,35 +1,103 @@
 const express = require('express');
+const { homes } = require('../../database/models/index');
 
 const router = express.Router();
 
 // POST /api/houses/
 router.post('', (req, res) => {
-  res.end(`endpoint: ${req.baseUrl}, ${req.method}`);
+  homes
+    .insert({})
+    .then(() => {
+      res.status(200);
+    })
+    .catch(() => {
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    });
 });
 
 // GET /api/houses/:id
 router.get('/:id', (req, res) => {
-  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+  const id = parseInt(req.params.id, 10);
+  homes
+    .getById(id)
+    .then((data) => {
+      res.status(200);
+      res.send(JSON.stringify(data));
+    })
+    .catch(() => {
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    });
 });
 
 // PATCH /api/houses/:id
 router.patch('/:id', (req, res) => {
-  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+  const id = parseInt(req.params.id, 10);
+  homes
+    .updateById(id)
+    .then(() => {
+      res.status(200);
+    })
+    .catch(() => {
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    });
 });
 
 // DELETE /api/houses/:id
 router.delete('/:id', (req, res) => {
-  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+  const id = parseInt(req.params.id, 10);
+  homes
+    .deleteById(id)
+    .then(() => {
+      res.status(200);
+    })
+    .catch(() => {
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    });
 });
 
 // GET /api/houses/:id/plot
 router.get('/:id/plot', (req, res) => {
-  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+  const id = parseInt(req.params.id, 10);
+  homes
+    .getPlotsById(id)
+    .then((data) => {
+      res.status(200);
+      res.send(JSON.stringify(data));
+    })
+    .catch(() => {
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    });
 });
 
 // POST /api/houses/:id/plot
 router.post('/:id/plot', (req, res) => {
-  res.end(`endpoint: ${req.baseUrl}, ${req.method}, ${JSON.stringify(req.params)}`);
+  const id = parseInt(req.params.id, 10);
+  homes
+    .insertPlotById(id)
+    .then(() => {
+      res.status(200);
+    })
+    .catch(() => {
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    });
 });
 
 module.exports = router;
