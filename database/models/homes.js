b2b2cda79cda56589cc5ac49100692a36f403c97
Samuel Deuter <SamuelDeuter@gmail.com>
2020-02-26 20:03:52 -0800

    insert model function

--- database/models/homes.js
+++ database/models/homes.js
@@ -3,11 +3,17 @@ const { client } = require('../../database/index');
 
 module.exports = {
   insert(home) {
-    // TODO
-    return Promise.reject(new Error('model method not implemented yet'));
+    const {
+      address, estimate, neighborhood, city,
+    } = home;
+    return client.query(
+      `INSERT INTO
+          service.homes (address, estimate, neighborhood_id, city_id)
+          VALUES(?, ?, ?, ?);`,
+      [address, estimate, neighborhood, city],
+    );
   },
   getById(id) {
-    // TODO
     return client.query('SELECT * FROM service.homes WHERE id = ?;', [id]);
   },
   updateById(id) {
